47fa43a5f629e283fe125c70701f3a41573c66ea
Wouter Steenmeijer <wouter@elgentos.nl>
2020-11-01 08:04:03 +0100

    Updated fuction

--- src/lambda/check-charge-point.js
+++ src/lambda/check-charge-point.js
@@ -6,17 +6,20 @@ var config = require("dotenv").config();
 export function handler(event, context, callback) {
 
   // Set the root URL according to the Netlify site we are within
-  var rootURL =  process.env.URL + "/";
+  // chargepointId = chargepointid
+
+  var rootURL =  'https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal';
 
   // post the new route to the Routes form
-  request.post({'url': rootURL, 'formData': payload }, function(err, httpResponse, body) {
-    var statusMessage;
+  request.post({'url': rootURL}, function(err, httpResponse, body) {
+    var statusMessage = true;
     if (statusMessage) {
       statusMessage = "Je laadpaal is vrij! Go Go Go";
     } else {
       statusMessage = "Chips, de laadpaal is bezet";
+      statusMessage = httpResponse;
     }
-    
+
     // tell the user what their shortcode will be
     return callback(null, {
       statusCode: 200,
