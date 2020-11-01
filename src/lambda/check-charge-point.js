6a959042b2b5f941cd60ba6a2b9681e319a71bc8
Wouter Steenmeijer <wouter@elgentos.nl>
2020-11-01 17:59:09 +0100

    Updated with multiple charging points

--- src/lambda/check-charge-point.js
+++ src/lambda/check-charge-point.js
@@ -1,39 +1,43 @@
 'use strict';
 
-var request = require("request");
-var config = require("dotenv").config();
+var axios = require("axios");
 
 export function handler(event, context, callback) {
 
-  // Set the root URL according to the Netlify site we are within
-  // chargepointId = chargepointid
+  const spotUrls = ['https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal',
+    'https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal',
+    'https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal'
+  ];
 
-  // Coendersweg 1
-  var rootURL =  'https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal';
 
-  //Helperbrink
-  var bestspot = '';
+  let spotResults = [];
+    Promise.all(spotUrls.map(spotUrl =>
+        axios.get(spotUrl)
+    ))
+    .then(function (results) {
 
-  // Einde coendersweg
-  var semiBestSpot = '';
+        results.map(function(result) {
+            spotResults.push(result.data.data[0].status);
+        });
 
-  // post the new route to the Routes form
-  request.get({'url': rootURL}, function(err, httpResponse, body) {
-    var statusMessage = JSON.parse(body);
+        let returnMessage = 'Alles bezet, zoek een andere met oplaadpalen.nl';
+        if (spotResults[2].available === 1) {
+            returnMessage = 'Kloten, ga naar Engelse Park';
+        }
 
-    if (statusMessage.data[0].status.available > 0) {
-      statusMessage = "Je laadpaal is vrij! Go Go Go";
-    } else {
-      statusMessage = "Chips, de laadpaal is bezet";
-    }
+        if (spotResults[1].available === 1) {
+            returnMessage = 'Argh, ga naar einde Coendersweg';
+        }
 
-    // tell the user what their shortcode will be
-    return callback(null, {
-      statusCode: 200,
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({"message": statusMessage})
-    })
-  });
+        if (spotResults[0].available === 1) {
+            returnMessage = "Go Go Go. De beste is vrij!";
+        }
 
+        return callback(null, {
+            statusCode: 200,
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({"message": returnMessage})
+        })
+    });
 }
 
