9aff7c662a3e5461699fbfee9270f3ad2b39171b
Wouter Steenmeijer <wouter@elgentos.nl>
2020-11-01 08:25:02 +0100

    Updated function

--- src/lambda/check-charge-point.js
+++ src/lambda/check-charge-point.js
@@ -8,16 +8,23 @@ export function handler(event, context, callback) {
   // Set the root URL according to the Netlify site we are within
   // chargepointId = chargepointid
 
+  // Coendersweg 1
   var rootURL =  'https://www.oplaadpalen.nl/api/maplist/clusterset?box=53.19767480278687,6.583232581615448,53.19902288146508,6.585383713245393&zoom=19&accessType=public,company&available=available,charging&power=fast,normal';
 
+  //Helperbrink
+  var bestspot = '';
+
+  // Einde coendersweg
+  var semiBestSpot = '';
+
   // post the new route to the Routes form
-  request.post({'url': rootURL}, function(err, httpResponse, body) {
-    var statusMessage = true;
-    if (statusMessage) {
+  request.get({'url': rootURL}, function(err, httpResponse, body) {
+    var statusMessage = JSON.parse(body);
+
+    if (statusMessage.data[0].status.available > 0) {
       statusMessage = "Je laadpaal is vrij! Go Go Go";
     } else {
       statusMessage = "Chips, de laadpaal is bezet";
-      statusMessage = httpResponse;
     }
 
     // tell the user what their shortcode will be
